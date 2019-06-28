483585206a7317633a2ad06fa2e060d08b70a374
Colin Edwards <colin.p.edwards@gmail.com>
2019-06-28 12:13:24 -0400

    Update VaronisDisableUserDenySMB.ps1

--- VaronisDisableUserDenySMB.ps1
+++ VaronisDisableUserDenySMB.ps1
@@ -36,16 +36,13 @@ Import-Module ActiveDirectory
 #Disable the user's AD account
 Disable-ADAccount $ActingUser
 
-#Get the OS Version of the File Server
-$OSVersion = Get-WmiObject -Computer $FileServer -Class Win32_OperatingSystem | Select -expand Caption
-
 #Block SMB access on all shares on the file server
 Invoke-Command -ComputerName $FileServer -ScriptBlock { ForEach ($Share in Get-SmbShare) { Block-SmbShareAccess -Name $Share.Name -AccountName $args[0] -Force } } -Args $ActingUser
 
-#Create a PowerShell script that can be  used to unblock SMB access on all shares on the file server after the incident is investigated and resolved
+#Create a PowerShell script that can be used to unblock SMB access on all shares on the file server, and re-enable the user's AD account after the incident is investigated and resolved
 $UnblockScriptName = "D:\PowerShell Unblock SMB Shares Scripts\" + $ActingUser + $FileServer + ".ps1"
 Write-Output "<#" | Out-File -FilePath "$UnblockScriptName" -Append
-Write-Output "Script Automatically Generated to Enable AD Account and undo an SMB Block created by VaronisDisableUserDenySMB1.ps1" | Out-File -FilePath "$UnblockScriptName" -Append
+Write-Output "Script Automatically Generated to Enable AD Account and undo an SMB Block created by VaronisDisableUserDenySMB.ps1" | Out-File -FilePath "$UnblockScriptName" -Append
 Write-Output "#>" | Out-File -FilePath "$UnblockScriptName" -Append
 Write-Output "Invoke-Command -ComputerName $FileServer -ScriptBlock { ForEach (`$Share in Get-SmbShare) { Unblock-SmbShareAccess -Name `$Share.Name -AccountName `$args[0] -Force } } -Args $ActingUser" | Out-File -FilePath "$UnblockScriptName" -Append
 Write-Output "Enable-ADAccount $ActingUser" | Out-File -FilePath "$UnblockScriptName" -Append
